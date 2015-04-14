4f4b19340ea24e06bae491d80b6b315bca251da3
Chris Atkin <atkinchris@gmail.com>
2015-04-14 23:21:53 +0100

    Added basic grid of input enabled circles

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-var game = new Phaser.Game(500, 600, Phaser.AUTO, 'game_div');
+var game = new Phaser.Game( 500, 600, Phaser.AUTO, 'game_div', null, true );
 
 var main_state = {
+	preload: function() {
+		game.load.image( 'circle', 'assets/circle.png' );
+	},
+	create: function() {
+		var width = 5;
+		var spacing = 60;
+		var scale = 0.5;
+		var circles = game.add.group();
 
-    preload: function() {
-   
+		function onDown( sprite, pointer ) {
+			game.add.tween( sprite ).to( {
+				alpha: 1
+			}, 800, Phaser.Easing.Linear.None, true, 0, 0, true );
+		}
 
-// Load a sprite in the game
-// Parameters: name of the sprite, path to the image
-game.load.image('hello', 'assets/hello.png');  
-        // Everything in this function will be executed at the beginning. That’s where we usually load the game’s assets (images, sounds, etc.)
-    },
+		for ( var y = 0; y < width; y++ ) {
+			for ( var x = 0; x < width; x++ ) {
+				var circle = circles.create( x * spacing, y * spacing, 'circle' );
+				circle.inputEnabled = true;
+				circle.events.onInputDown.add( onDown, this );
+				circle.scale.x = scale;
+				circle.scale.y = scale;		
+				circle.alpha = 0.1;		
+			};
+		};
 
-    create: function() { 
-        // This function will be called after the preload function. Here we set up the game, display sprites, add labels, etc.
-   // Display a sprite on the screen
-// Parameters: x position, y position, name of the sprite
-this.hello_sprite = game.add.sprite(250, 300, 'hello');  
-    },
+		circles.x = Math.round( ( game.width - circles.width ) / 2 );
+		circles.y = Math.round( ( game.height - circles.height ) / 2 );
+	},
+	update: function() {
 
-    update: function() {
-       // Increase the angle of the sprite by one degree
-this.hello_sprite.angle += 1;   // This is where we will spend the most of our time. This function is called 60 times per second to update the game.
-    } 
+	}
 }
 
 // And finally we tell Phaser to add and start our 'main' state
-game.state.add('main', main_state);  
-game.state.start('main');
+game.state.add( 'main', main_state );
+game.state.start( 'main' );
