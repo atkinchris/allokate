e2c8ae2ed080ce3a1b6b8bf9956df4e00e494b37
Chris Atkin <atkinchris@gmail.com>
2015-04-15 16:24:19 +0100

    Switched to pure JS

--- main.js
+++ main.js
@@ -1,40 +1,64 @@
-var game = new Phaser.Game( 500, 600, Phaser.AUTO, 'game_div', null, true );
+function generatePuzzle() {
+	var myStringArray = new Array(width * width);
+	var arrayLength = myStringArray.length;
+	var total = 0;
 
-var main_state = {
-	preload: function() {
-		game.load.image( 'circle', 'assets/circle.png' );
-	},
-	create: function() {
-		var width = 5;
-		var spacing = 60;
-		var scale = 0.5;
-		var circles = game.add.group();
-
-		function onDown( sprite, pointer ) {
-			game.add.tween( sprite ).to( {
-				alpha: 1
-			}, 800, Phaser.Easing.Linear.None, true, 0, 0, true );
+	while (total < width) {
+		for (var i = 0; i < arrayLength; i++) {
+			myStringArray[i] = Math.round(Math.random());
 		}
+		total = myStringArray.reduce(function(a, b) {
+			return a + b;
+		});
+	}
 
-		for ( var y = 0; y < width; y++ ) {
-			for ( var x = 0; x < width; x++ ) {
-				var circle = circles.create( x * spacing, y * spacing, 'circle' );
-				circle.inputEnabled = true;
-				circle.events.onInputDown.add( onDown, this );
-				circle.scale.x = scale;
-				circle.scale.y = scale;		
-				circle.alpha = 0.1;		
-			};
-		};
+	return myStringArray;
+}
 
-		circles.x = Math.round( ( game.width - circles.width ) / 2 );
-		circles.y = Math.round( ( game.height - circles.height ) / 2 );
-	},
-	update: function() {
+function updateCounts(puzzle) {
+	columns = columns.map(Number.prototype.valueOf, 0);
+	rows = rows.map(Number.prototype.valueOf, 0);
+	for (var i = 0; i < puzzle.length; i++) {
+		var value = puzzle[i];
+		rows[Math.floor(i / width)] += value;
+		columns[i % width] += value;
+	}
 
+	for (var i = 0; i < width; i++) {
+		columnElements[i].innerHTML = columns[i];
+		rowElements[i].innerHTML = rows[i];
 	}
 }
 
-// And finally we tell Phaser to add and start our 'main' state
-game.state.add( 'main', main_state );
-game.state.start( 'main' );
+function zeroArray(length) {
+	return Array.apply(null, new Array(length)).map(Number.prototype.valueOf, 0);
+}
+
+function togglePiece(position) {
+	var value = pieceArray[position];
+	pieceArray[position] = value == 0 ? 1 : 0;
+	updateCounts(pieceArray);
+}
+
+function setupPieces() {
+	var g = document.getElementsByClassName("piece");
+	for (var i = 0, len = g.length; i < len; i++) {
+		(function(index) {
+			g[i].onclick = function() {
+				togglePiece(index);
+				this.classList.toggle('selected');
+			}
+		})(i);
+	}
+}
+
+var width = 5;
+
+var puzzleArray = generatePuzzle();
+var pieceArray = zeroArray(width * width);
+var columnElements = document.getElementsByClassName("column");
+var rowElements = document.getElementsByClassName("row");
+var rows = zeroArray(width);
+var columns = zeroArray(width);
+setupPieces();
+updateCounts(puzzleArray);
